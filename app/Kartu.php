6d95170172724231237b967cf8fb8bde37131f0f
muflihunaf <firmanedam19@gmail.com>
2018-10-17 12:37:23 +0700

    Penambahan Bulan

---
+++ app/Kartu.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kartu extends Model
+{
+    protected $table = 'kartu';
+    protected $primaryKey = 'id_kartu';
+    protected $fillable = ['bulan','status','id_siswa'];
+}

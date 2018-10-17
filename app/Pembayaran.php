6ef0f92b1ea5c0d350fbe286b44d96e6f78d1ffd
muflihunaf <firmanedam19@gmail.com>
2018-10-17 12:38:08 +0700

    Penambahan Pembayaran

---
+++ app/Pembayaran.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pembayaran extends Model
+{
+    protected $table = 'pembayaran';
+    protected $primaryKey = 'id_pembayaran';
+    protected $fillable = ['bulan','jumlah','id_siswa'];
+}

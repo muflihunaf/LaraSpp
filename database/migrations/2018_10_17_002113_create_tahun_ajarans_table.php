7e0fe3306934f5b55d07b14365cb1663d58f3856
muflihunaf <firmanedam19@gmail.com>
2018-10-17 08:50:03 +0700

    Penambahan Tahun Ajaran

---
+++ database/migrations/2018_10_17_002113_create_tahun_ajarans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTahunAjaransTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tahun_ajaran', function (Blueprint $table) {
+            $table->increments('id_tahun');
+            $table->string('tahun',20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tahun_ajarans');
+    }
+}

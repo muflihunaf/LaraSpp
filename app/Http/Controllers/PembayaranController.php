e44c4f325b6f2e0634f30ae25efb47ed9d8b85e7
muflihunaf <firmanedam19@gmail.com>
2018-10-17 12:41:03 +0700

    Perubahan Pada Controller

--- app/Http/Controllers/PembayaranController.php
+++ app/Http/Controllers/PembayaranController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Pembayaran;
 use App\Siswa;
 Use App\Kelas;
+use App\Kartu;
 use DB;
 
 class PembayaranController extends Controller
@@ -23,8 +25,28 @@ class PembayaranController extends Controller
     public function daftar($id)
     {
         $siswa = Siswa::find($id)->join('kelas','kelas.id_kelas', '=', 'siswa.id_kelas')->where('siswa.id_siswa', '=', $id)->first();
-        
-        return view('bayar.daftar',compact('siswa'));
+        $kartu = Kartu::where('id_siswa','=',$siswa->id_siswa,'AND', 'status','=','Belum Dibayar')->get();
+        return view('bayar.daftar',compact('siswa','kartu'));
+    }
+
+    public function ulang(Request $request, $id)
+    {
+        $bulan = ['Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember'];
+        $siswa = Siswa::find($id);
+        for ($i=0; $i < 12; $i++) { 
+            $kartu = new Kartu;
+            $kartu->bulan = $bulan[$i];
+            $kartu->status = 'Belum Dibayar';
+            $kartu->id_siswa = $siswa->id_siswa;
+            $kartu->save();
+        }
+        return redirect()->back();
+    }
+    public function lunas(Request $request,$id)
+    {
+        $siswa = Siswa::find($id);
+        $bayar = new Pembayaran;
+
     }
 
 }

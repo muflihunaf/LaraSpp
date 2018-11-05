a4e7e2ad6c825b713f53c9aea12a52f0e33dea12
muflihunaf <firmanedam19@gmail.com>
2018-11-05 10:39:25 +0700

    Perubahan Tempat Aja

--- resources/views/bayar/daftar.blade.php
+++ resources/views/bayar/daftar.blade.php
@@ -2,75 +2,89 @@
 
 @section('content')
 
-<div class="row">
-    <div class="col-md-12">
+    <div class="row">
+        <div class="col-md-12">
 
-        <label>Nama Siswa :</label>
-        <label> {{ $siswa->nama }} </label>
+            <label>Nisn :</label>
+            <label> {{ $siswa->nisn }} </label>
+        </div>
     </div>
-</div>
-<div class="row">
-    <div class="col-md-12">
+    <div class="row">
+        <div class="col-md-12">
+            <label>Nama Siswa :</label>
+            <label> {{ $siswa->nama }} </label>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
 
-        <label>Kelas :</label>
-        <label> {{ $siswa->kelas }} </label>
+            <label>Kelas :</label>
+            <label> {{ $siswa->kelas }} </label>
+        </div>
     </div>
-</div>
-<div class="row">
-    <div class="col-md-12">
-
-        <label>Nisn :</label>
-        <label> {{ $siswa->nisn }} </label>
+    <hr>
+    <div class="row" style="">
+        <div class="col-md-12">
+            <form action=" {{ route('bayar.daftarulang', $siswa->id_siswa) }} " method="post">
+                {{ csrf_field() }}
+                <select name="id_tahun">
+                    @foreach ($tahun as $ajaran)
+                    <option value="{{ $ajaran->id_tahun }} "> {{ $ajaran->tahun }} </option>
+                    @endforeach
+                </select>
+                <button type="submit">Daftar</button>
+            </form>
+        </div>
     </div>
-</div>
-<hr>
-<div class="row" style="">
-    <div class="col-md-12">
-    <form action=" {{ route('bayar.daftarulang', $siswa->id_siswa) }} " method="post">
-        {{ csrf_field() }}
-        <button type="submit">Daftar</button>
-    </form>
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-12">
+    <div class="row">
+        <div class="col-md-12">
             <table class="table table-striped" id="siswa">
-            <thead>
-                <tr>
-                    <td>No </td>
-                    <td>Bulan</td>
-                    <td>Status</td>
-                    <td>Aksi</td>
-                </tr>
-            </thead>
-            <tbody>
+                <thead>
+                    <tr>
+                        <td></td>
+                        <td>No </td>
+                        <td>Bulan</td>
+                        <td>Status</td>
+                        <td>Tahun Ajaran</td>
+                        <td>Aksi</td>
+                    </tr>
+                </thead>
+                <tbody>
                     @php
                     $no = 1;
-                @endphp
-            @foreach ($kartu as $item)
-            @php
-                $status = "Belum Dibayar"    
-            @endphp
-            @if ($item->status == $status)
+                    @endphp
+                    @foreach ($kartu as $item)
+                    @php
+                    $status = "Belum Dibayar"
+                    @endphp
+                    
+                    <tr>
+                        <td><input type="checkbox" name="" id=""></td>
+                        <td> {{ $no++ }} </td>
+                        <td> {{ $item->bulan }} </td>
+                        <td> {{ $item->status }} </td>
+                        <td> {{ $item->tahun }} </td>
+                        {{-- <td><a href="pembayaran/{{ $item->id_kartu }}/lunas" onclick="event.preventDefault();document.getElementById('lunas').submit(); "
+                                class="btn btn-primary">Bayar</a></td> --}}
+                        @if ($item->status == $status)
+                        <td>
+                            <form action="/pembayaran/{{ $item->id_kartu }}/lunas" id="lunas" method="post">
+                                {{ csrf_field() }}
+                                <input type="hidden" name="status" value="Lunas">
+                                <input type="submit" class="btn btn-primary" value="Bayar">
+                            </form>
+                        </td>
+                        @else
+                        <td> <a href="" class="btn btn-primary">Cetak Bukti</a> </td>
+                    </tr>
+                    
+                    
+                    @endif
+                    @endforeach
+                </tbody>
                 
-            
-            <tr>
-                <td> {{ $no++ }} </td>
-                <td> {{ $item->bulan }}  </td>
-                <td> {{ $item->status }} </td>
-                {{-- <td><a href="pembayaran/{{ $item->id_kartu }}/lunas" onclick="event.preventDefault();document.getElementById('lunas').submit(); " class="btn btn-primary">Bayar</a></td> --}}
-                <td><form action="/pembayaran/{{ $item->id_kartu }}/lunas" id="lunas" method="post">
-                    {{ csrf_field() }}
-                    <input type="hidden" name="status" value="Lunas">
-                    <input type="submit">
-                </form>
-            </td>
-            </tr>
-            @endif
-            @endforeach
-        </tbody>
-        </table>
+            </table>
+        </div>
     </div>
-</div>
 
 @endsection
